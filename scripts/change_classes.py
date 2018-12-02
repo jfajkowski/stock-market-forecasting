e33b1503ecb0ba2810b22c969f2f40d519b6f88f
unknown <motyka.tomasz.1995@gmail.com>
2018-12-02 13:16:17 +0100

    Add script for changing initial classes

---
+++ scripts/change_classes.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+ChangeColumn = 'Change'
+ClassColumn = 'Class'
+
+#load data
+djia = pd.read_csv("DJIA_table.csv", sep=',')
+combined = pd.read_csv("Combined_News_DJIA.csv", sep=',')
+
+#calculate relative change in index value
+diff = ((djia['Open'] - djia['Close']) / djia['Open'])
+djia[ChangeColumn] = diff
+
+#assign new classes
+djia.loc[djia[ChangeColumn] < -0.025, ClassColumn] = 1
+djia.loc[(djia[ChangeColumn] >= -0.025) & (djia[ChangeColumn] < -0.005), ClassColumn] = 2
+djia.loc[(djia[ChangeColumn] >= -0.005) & (djia[ChangeColumn] < 0.005), ClassColumn] = 3
+djia.loc[(djia[ChangeColumn] >= 0.005) & (djia[ChangeColumn] < 0.025), ClassColumn] = 4
+djia.loc[0.025 <= djia[ChangeColumn], ClassColumn] = 5
+
+#join new classes with combined file
+combined = combined.join(djia.set_index('Date')[ClassColumn], on='Date')
+combined = combined.drop(['Label'], axis=1)
+
+combined.to_csv(path_or_buf='Outcome.csv', index=False)
+
+# diff = diff.sort_values().reset_index(drop=True)
+# diff.plot()
+# plt.show()
+
