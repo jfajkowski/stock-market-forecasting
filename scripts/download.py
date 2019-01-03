1eec83a198a2a283a98b9750b64ec37b0a10d10f
jfajkowski <j.fajkowski@stud.elka.pw.edu.pl>
2019-01-03 23:50:53 +0100

    Add download link for Google's Word2Vec model

--- scripts/download.py
+++ scripts/download.py
@@ -3,3 +3,7 @@ import urllib.request
 # FastText vectors
 urllib.request.urlretrieve('https://s3-us-west-1.amazonaws.com/fasttext-vectors/cc.en.300.vec.gz',
                            './data/external/cc.en.300.vec.gz')
+
+# Word2Vec vectors
+urllib.request.urlretrieve('https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/edit?usp=sharing',
+                           './data/external/GoogleNews-vectors-negative300.bin')
