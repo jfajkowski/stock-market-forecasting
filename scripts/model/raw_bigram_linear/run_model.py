a39dcf8b87f3566052dcb44ec785d1d9eef34b0f
jfajkowski <j.fajkowski@stud.elka.pw.edu.pl>
2019-01-09 22:19:36 +0100

    Socket communication with model

--- scripts/model/raw_bigram_linear/run_model.py
+++ scripts/model/raw_bigram_linear/run_model.py
@@ -1,19 +1,54 @@
 import json
+import logging
 import pickle
-import sys
+import socket
 
+TCP_HOST = "127.0.0.1"
+TCP_PORT = 10000
+BUFFER_SIZE = 1024
+QUEUE_SIZE = 128
+
+logging.basicConfig(format='[%(asctime)s][%(levelname)s] %(name)s: %(message)s', level=logging.INFO)
+
+# Load serialized model
 with open('./models/raw_bigram_linear.vcr', mode='rb') as vcr_file:
     vectorizer = pickle.load(vcr_file)
 with open('./models/raw_bigram_linear.clf', mode='rb') as clf_file:
     classifier = pickle.load(clf_file)
 
-# model_input_line = '{"indexHistory":[{"date":"2016-06-30","openValue":2.0,"closeValue":3.0},{"date":"2016-07-01","openValue":1.0,"closeValue":2.0}],"articles":[{"date":"2016-06-29","header":"Test header 3"},{"date":"2016-06-30","header":"Test header 2"},{"date":"2016-07-01","header":"Test header 1"}]}'
-model_input_line = next(sys.stdin)
-model_input = json.loads(model_input_line)
+# Serve it on TCP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_HOST, TCP_PORT))
+s.listen(QUEUE_SIZE)
 
-articles = sorted(model_input['articles'], key=lambda x: x['date'])
+logging.info('Model running on %s:%s', TCP_HOST, TCP_PORT)
 
-raw = list(map(lambda x: x['header'], articles))
-X = vectorizer.transform([raw[-1]])
-y = classifier.predict_proba(X)
-print(y[0])
+while True:
+    try:
+        connection, address = s.accept()
+
+        logging.info('%s:%s connected', *address)
+
+        data = b''
+        while True:
+            buffer = connection.recv(BUFFER_SIZE)
+            data += buffer
+            if len(buffer) < BUFFER_SIZE:
+                break
+
+        model_input_line = data.decode()
+        logging.info('%s:%s received %s', *address, model_input_line)
+        model_input = json.loads(model_input_line)
+
+        articles = sorted(model_input['articles'], key=lambda x: x['date'])
+
+        raw = list(map(lambda x: x['header'], articles))
+        X = vectorizer.transform([raw[-1]])
+        y = classifier.predict_proba(X)
+        model_output = y[0]
+        model_output_line = str(model_output)
+        connection.sendall(model_output_line.encode())
+        logging.info('%s:%s sent %s', *address, model_output_line)
+        connection.close()
+    except Exception as e:
+        logging.error(e)
